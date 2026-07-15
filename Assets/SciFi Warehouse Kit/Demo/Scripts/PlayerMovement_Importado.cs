0e85a72097746fb08bea23b8094ff460ebc158f8
OsvaldoMezaHerrera <20243ds051@utez.edu.mx>
2026-07-14 22:11:50 -0600

    avance nivel 1

--- Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement_Importado.cs
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement_Importado.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement_Importado : MonoBehaviour
 {
 
     public CharacterController controller;
